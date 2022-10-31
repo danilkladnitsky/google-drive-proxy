0398bb4bc3bb7bf0d86aabdd410e99af3329bc06
Danil Kladnitsky <danyakladnitsky@gmail.com>
2022-10-31 20:03:56 +0300

    add docker compose files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { configService } from './config/typeorm.config';
 import { StorageModule } from './endpoints/storage/storage.module';
 
+
+const DatabaseModule = TypeOrmModule.forRoot(configService.getTypeOrmConfig());
+
 @Module({
-  imports: [StorageModule],
+  imports: [DatabaseModule, StorageModule],
   controllers: [AppController],
 })
 export class AppModule {}
